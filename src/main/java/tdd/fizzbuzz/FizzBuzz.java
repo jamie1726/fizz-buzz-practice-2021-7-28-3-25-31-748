50d0a81f1512264b20eb6cb1ed88d796a8d43369
JAMIE GABRIELLE LAZARO (OPS-IRIS-ISD-OOCLL/MNL) <LAZARJA@OOCL.COM>
2022-09-01 15:18:18 +0800

    refactor: change if-else to if statement to implement combination if number is multiple of 5 and 7

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,21 +2,22 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    public String countOff(int order){
+    public String countOff(int order) {
         String fizz = "Fizz";
         String buzz = "Buzz";
         String whizz = "Whizz";
+        String output = "";
 
-        if (order%3==0){
-            return fizz;
-        } else if (order%5==0) {
-            return buzz;
-        } else if (order%7==0) {
-            return whizz;
-        } else {
-            return String.valueOf(order);
-        }
+        if (order % 3 == 0)
+            output += fizz;
+        if (order % 5 == 0)
+            output += buzz;
+        if (order % 7 == 0)
+            output += whizz;
+        if (output.length() == 0)
+            output = String.valueOf(order);
 
+        return output;
     }
 
 }
