48f21231adde4a472db0c5a0916c2703e9fca0f1
JAMIE GABRIELLE LAZARO (OPS-IRIS-ISD-OOCLL/MNL) <LAZARJA@OOCL.COM>
2022-09-01 14:23:02 +0800

    test: Return 1 if given number is 1

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,10 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String countOff(int order){
+
+        //return String.valueOf(order);
+    }
+
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import com.sun.org.apache.xpath.internal.operations.Gt;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void should_return_1_when_count_off_given_number_1() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+        //when
+        String actual = fizzBuzz.countOff(1);
+        //then
+        assertEquals(expected, actual);
+    }
+    
 }
