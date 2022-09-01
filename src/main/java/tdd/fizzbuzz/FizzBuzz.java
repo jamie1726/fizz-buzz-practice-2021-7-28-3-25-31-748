9a30a0a9c0f7bfe5d2b1b4048f85f536bca792fa
JAMIE GABRIELLE LAZARO (OPS-IRIS-ISD-OOCLL/MNL) <LAZARJA@OOCL.COM>
2022-09-01 14:33:30 +0800

    refactor: Will return Fizz when given is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,8 +3,15 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int order){
+        String fizz = "Fizz";
+        String buzz = "Buzz";
+        if (order%3==0){
+            return fizz;
+        }
+        else {
+            return String.valueOf(order);
+        }
 
-        return String.valueOf(order);
     }
 
 }
