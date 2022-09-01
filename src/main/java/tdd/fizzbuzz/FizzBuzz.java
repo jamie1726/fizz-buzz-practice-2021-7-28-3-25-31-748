c943f3ed69f03792a458f012be9e4228f1a4504d
JAMIE GABRIELLE LAZARO (OPS-IRIS-ISD-OOCLL/MNL) <LAZARJA@OOCL.COM>
2022-09-01 14:37:49 +0800

    refactor: return buzz if given multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,10 +5,12 @@ public class FizzBuzz {
     public String countOff(int order){
         String fizz = "Fizz";
         String buzz = "Buzz";
+        String whizz = "Whizz";
         if (order%3==0){
             return fizz;
-        }
-        else {
+        } else if (order%5==0) {
+            return  buzz;
+        } else {
             return String.valueOf(order);
         }
 
